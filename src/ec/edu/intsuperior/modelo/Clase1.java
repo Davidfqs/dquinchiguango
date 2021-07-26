6e5949c38232e36077061415af70a7b11d9346fc
David <David@David>
2021-07-26 18:58:56 -0500

    Primera version 1.0

---
+++ src/ec/edu/intsuperior/modelo/Clase1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author David
+ */
+public class Clase1 {
+    public static void dormir(){
+        System.out.println("Es hora de dormir");
+    }
+    public static int suma(){
+        int x;
+        x=2;
+        int y; 
+        y=7;
+        int z;
+        z=x+y;
+        return z;
+    }
+    public void salir(){
+        System.out.println("Hora de Salir");
+    }
+    public boolean comparar(){
+        return 5==8;
+    }
+}
